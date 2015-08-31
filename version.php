203f0ad539655e2098d43bf4f30940e799a49823
Mark <mmichaelsen678@gmail.com>
2015-08-31 00:22:05 -0300

    Archivos para instalacion de plug-in, mas creacion de tabla xml

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+* @package local
+* @subpackage pluginboletas
+* @copyright 2015 Mark Michaelsen michaelsen678@gmail.com
+* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3
+or later*/
+defined('MOODLE_INTERNAL') || die();
+$plugin->pluginname = "local_pluginboletas";
+$plugin->component = 'local_pluginboletas';
+$plugin->version = '2015083002';
