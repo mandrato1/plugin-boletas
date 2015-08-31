090e4f66b621e0ebec0894dd7355f149231480c3
Mark <mmichaelsen678@gmail.com>
2015-08-31 00:52:57 -0300

    Se crea la interfaz basica de la pagina, para ser editada.

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,36 +1,22 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 /**
  * This file keeps track of upgrades to the evaluaciones block
 *
 * Sometimes, changes between versions involve alterations to
-database structures
+*database structures
 * and other major things that may break installations.
 *
 * The upgrade function in this file will attempt to perform all
-the necessary
+*the necessary
 * actions to upgrade your older installation to the current
-version.
+*version.
 *
 * If there's something it cannot do itself, it will tell you
-what you need to do.
+*what you need to do.
 *
 * The commands in here will all be database-neutral, using the
-methods of
+*methods of
 * database_manager class
 *
 * Please do not forget to use upgrade_set_timeout()
@@ -40,14 +26,14 @@ methods of
 * @package local_pluginboletas
 * @copyright 2015 Mark Michaelsen
 * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3
-or later
+*or later
 */
 /**
  *
 * @param int $oldversion
 * @param object $block
 */
-function xmldb_local_nombreplugin_upgrade($oldversion) {
+function xmldb_local_pluginboletas_upgrade($oldversion) {
 	global $CFG, $DB;
 	$dbman = $DB->get_manager();
 	
--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
 
+require_once(dirname(dirname(dirname(__FILE__)))."/config.php");
+
+$context = context_system::instance();
+
+$urlprinters = new moodle_url("/local/pluginboletas/index.php");
+// Page navigation and URL settings
+$PAGE->set_url($urlprinters);
+$PAGE->set_context($context);
+$PAGE->set_pagelayout("standard");
+
+$PAGE->set_title("HOLA MUNDO");
+$PAGE->set_heading("Este es un heading. HOLA!!");
+echo $OUTPUT->header();
+
+echo "Aqui veran las boletas... algun dia"; //PROGRAMAR AQUI!!!!
+
+echo $OUTPUT->footer();
